7cfe10520530d5372108aa0a3905c71dce3050fa
barbishop <vladimir.a.fedorenko@gmail.com>
2024-05-27 10:47:40 +0300

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,19 @@
-export const BASE_URL = 'https://pixabay.com/api/';
-export const API_KEY = '44085947-f129bb4457b00ce47b6639669';
+import axios from 'axios';
 
-export function fetchData(query) {
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&per_page=15&safesearch=true&orientation=horizontal`
-  ).then(responce => {
-    if (!responce.ok) {
-      throw new Error(response.statusText);
-    }
-    return responce.json();
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '44085947-f129bb4457b00ce47b6639669';
+
+export async function fetchData(query, page, perPage) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      per_page: perPage,
+      page: page,
+      safesearch: true,
+      orientation: 'horizontal',
+    },
   });
+  return response.data;
 }
--- src/main.js
+++ src/main.js
@@ -9,11 +9,12 @@ import {
   showMessage,
   showLoader,
   hideLoader,
-} from './js/render-function.js';
+} from './js/render-functions';
 
 const form = document.querySelector('.form');
 const queryInput = document.querySelector('input[name="query"]');
 const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
 iziToast.settings({
   messageColor: '#fafafb',
@@ -23,47 +24,86 @@ iziToast.settings({
   close: false,
 });
 
-let lightbox;
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+let query = '';
+let page = 1;
+const perPage = 15;
+let images;
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', async event => {
   event.preventDefault();
-
-  const query = queryInput.value.trim();
-
+  query = queryInput.value.trim();
   if (query === '') {
     showMessage('Please enter a search query.');
     return;
   }
-
+  loadMoreBtn.classList.add('visually-hidden');
   gallery.innerHTML = '';
+  page = 1;
 
   showLoader();
 
-  fetchData(queryInput.value.trim())
-    .then(images => {
-      if (images.hits.length === 0) {
-        showMessage(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-        return;
-      } else {
-        renderGallery(images);
-      }
-      if (!lightbox) {
-        lightbox = new SimpleLightbox('.gallery a', {
-          captionsData: 'alt',
-          captionPosition: 'bottom',
-          captionDelay: 250,
-        });
-      } else {
-        lightbox.refresh();
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
+  try {
+    images = await fetchData(query, page, perPage);
+    if (images.hits.length === 0) {
+      showMessage(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
       hideLoader();
-      form.reset();
+      console.log(
+        '🚀 ~ form.addEventListener ~ images.hits.length:',
+        images.hits.length
+      );
+      return;
+    }
+
+    if (images.hits.length < 15) {
+      loadMoreBtn.classList.add('visually-hidden');
+    } else {
+      loadMoreBtn.classList.remove('visually-hidden');
+    }
+    renderGallery(images.hits);
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+    form.reset();
+  }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  try {
+    loadMoreBtn.classList.add('visually-hidden');
+    showLoader();
+
+    images = await fetchData(query, page, perPage);
+    renderGallery(images.hits);
+    lightbox.refresh();
+
+    const galleryCard = document
+      .querySelector('.gallery-item')
+      .getBoundingClientRect();
+    const cardHeight = galleryCard.height;
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
     });
+
+    if (images.total <= Math.ceil(page * perPage)) {
+      loadMoreBtn.classList.add('visually-hidden');
+      showMessage("We're sorry, but you've reached the end of search results.");
+      return;
+    }
+    loadMoreBtn.classList.remove('visually-hidden');
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
 });
